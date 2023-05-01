a861f92e4fd36fc9b54ba8888c8d7b528a9c0732
nikitagolovanov <119881857+NikitaGolovanov@users.noreply.github.com>
2023-05-01 16:03:23 +0300

    feat: DOM final version

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+var tittle = document.createElement("h1")
+tittle.innerText = "RSS Virtual Keyboard"
+tittle.className = "Task_Name"
+
 var pole = document.createElement("textarea")
 pole.className = "pole_dlya_texta"
+pole.rows = 20
+pole.cols = 200
 
 var klava = document.createElement("div")
 klava.className = "klaviatura"
@@ -19,4 +25,74 @@ row_four.className = "row_four"
 var row_five = document.createElement("div")
 row_five.className = "row_five"
 
+var knopki = []
+for (let i = 0; i < 57; ++i){
+   knopki[i] = document.createElement("butt")
+   knopki[i].className = "butt"
+}
+
+var tab = document.createElement("butt")
+tab.className = "tab"
+
+var caps = document.createElement("butt")
+caps.className = "caps"
+
+var shift_l = document.createElement("butt")
+shift_l.className = "shift_l"
+
+var space = document.createElement("butt")
+space.className = "space"
+
+var backspace = document.createElement("butt")
+backspace.className = "backspace"
+
+var enter = document.createElement("butt")
+enter.className = "enter"
+
+var shift_r = document.createElement("butt")
+shift_r.className = "shift_r"
+
+for (let i = 0; i < 13; ++i){
+    row_one.appendChild(knopki[i])
+}
+row_one.appendChild(backspace)
+klava.appendChild(row_one)
+
+row_two.appendChild(tab)
+for (let i = 13; i < 27; ++i){
+    row_two.appendChild(knopki[i])
+}
+klava.appendChild(row_two)
+
+row_three.appendChild(caps)
+for (let i = 27; i < 38; ++i){
+    row_three.appendChild(knopki[i])
+}
+row_three.appendChild(enter)
+klava.appendChild(row_three)
+
+row_four.appendChild(shift_l)
+for (let i = 38; i < 49; ++i){
+    row_four.appendChild(knopki[i])
+}
+row_four.appendChild(shift_r)
+klava.appendChild(row_four)
+
+for (let i = 49; i < 52; ++i){
+    row_five.appendChild(knopki[i])
+}
+row_five.appendChild(space)
+for (let i = 52; i < 57; ++i){
+    row_five.appendChild(knopki[i])
+}
+klava.appendChild(row_five)
+
+var podskazka = document.createElement("h2")
+podskazka.innerText = "Windows OS\nTo switch language {Left Shift + Left Alt}"
+podskazka.className = "podskazka"
+
+document.body.appendChild(tittle)
+document.body.appendChild(pole)
+document.body.appendChild(klava)
+document.body.appendChild(podskazka)
 
