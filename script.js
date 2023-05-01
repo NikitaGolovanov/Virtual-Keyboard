5caf13833d90949bbbec4883229456de97d3c4f5
nikitagolovanov <119881857+NikitaGolovanov@users.noreply.github.com>
2023-05-01 14:52:27 +0300

    feat: add DOM

---
+++ script.js
@@ -0,0 +1,22 @@
+var pole = document.createElement("textarea")
+pole.className = "pole_dlya_texta"
+
+var klava = document.createElement("div")
+klava.className = "klaviatura"
+
+var row_one = document.createElement("div")
+row_one.className = "row_one"
+
+var row_two = document.createElement("div")
+row_two.className = "row_two"
+
+var row_three = document.createElement("div")
+row_three.className = "row_three"
+
+var row_four = document.createElement("div")
+row_four.className = "row_four"
+
+var row_five = document.createElement("div")
+row_five.className = "row_five"
+
+
